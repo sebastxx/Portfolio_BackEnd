94e53c39087ee6b1840d328073f1b8451c7caa6b
sebastxx <91297447+sebastxx@users.noreply.github.com>
2022-11-16 04:06:58 -0300

    Add files via upload

---
+++ src/main/java/com/portfolio/portfolioAP/Service/Shys.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.portfolioAP.Service;
+
+import com.portfolio.portfolioAP.Entity.hys;
+import com.portfolio.portfolioAP.Repository.Rhys;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
+public class Shys {
+    @Autowired
+    Rhys rhys;
+    
+    public List<hys> list(){
+        return rhys.findAll();
+    }
+    
+    public Optional<hys> getOne(int id){
+        return rhys.findById(id);
+    }
+    
+    public Optional<hys> getByNombre(String nombre){
+        return rhys.findByNombre(nombre);
+    }
+    
+    public void save(hys skill){
+        rhys.save(skill);
+    }
+    
+    public void delete(int id){
+        rhys.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rhys.existsById(id);
+    }
+    
+    public boolean existsByNombre(String nombre){
+        return rhys.existsByNombre(nombre);
+    }
+
+    // public void save(com.portfolio.portfolioAP.Entity.hys hYs) {
+    // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    // }
+}
