dd429cd12d51457ebbdd6b845db5b7cf3e35b7af
sebastxx <91297447+sebastxx@users.noreply.github.com>
2022-11-16 04:05:59 -0300

    Add files via upload

---
+++ Portfolio_BackEnd/README.md
@@ -0,0 +1,2 @@
+# Portfolio_BackEnd
+ Argentina Programa
---
+++ sebastxx-cv/src/main/java/com/portfolio/portfolioAP/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.portfolioAP.Repository;
+
+import com.portfolio.portfolioAP.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
